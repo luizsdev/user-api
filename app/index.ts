45c706fd68719edd361bf52d5402d1ede0f923be
luizsdev <luizsdev@gmail.com>
2022-09-20 03:27:26 -0300

    update

--- app/index.ts
+++ app/index.ts
@@ -5,11 +5,7 @@ const PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use("/", router);
-const options: cors.CorsOptions = {
-  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin: "*",
-};
-app.use(cors(options));
+app.use(cors())
 app.listen(PORT, async () => {
   console.log("Server listening on port " + PORT);
 });
