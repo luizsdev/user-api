e49e54d7ac835a4b314088bba9d92c74cd38ebd0
luizsdev <luizsdev@gmail.com>
2022-09-20 00:19:01 -0300

    updt

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import  cors from "cors"
 import router from "./Routes/userRoutes"
 const PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use("/", router);
-
+app.use(cors())
 app.listen(PORT, async () => {
   console.log("Server listening on port " + PORT);
 });
