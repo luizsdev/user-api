878fae541bb4c91995928af3b2f60345288ee682
luizsdev <luizsdev@gmail.com>
2022-09-20 00:36:06 -0300

    update

--- app/index.ts
+++ app/index.ts
@@ -5,10 +5,7 @@ const PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use("/", router);
-app.use(cors())
-app.options("*", cors({ origin: 'http://localhost:5173', optionsSuccessStatus: 200 }));
-
-app.use(cors({ origin: "http://localhost:5173", optionsSuccessStatus: 200 }));
+app.use(cors)
 app.listen(PORT, async () => {
   console.log("Server listening on port " + PORT);
 });
