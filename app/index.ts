0ec344c21f30d13cb4df8ba3661f366a243db298
luizsdev <luizsdev@gmail.com>
2022-09-19 21:50:00 -0300

    update

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import router from "./Routes/userRoutes"
+const PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use("/", router);
 
 app.listen(3000, async () => {
-  console.log("Server listening on port " + 3000);
+  console.log("Server listening on port " + PORT);
 });
 
 export default app;
