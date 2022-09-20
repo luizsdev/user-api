f34e1dce2ceb6eae2975fe6a05a80163e9c3ca38
luizsdev <luizsdev@gmail.com>
2022-09-20 00:38:58 -0300

    update

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,25 @@
 import express from "express";
-import  cors from "cors"
+import cors from 'cors';
 import router from "./Routes/userRoutes"
 const PORT = process.env.PORT || 3000
 const app = express();
 app.use(express.json());
 app.use("/", router);
-app.use(cors)
+const options: cors.CorsOptions = {
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: "http://localhost:5173",
+  preflightContinue: false,
+};
+router.use(cors(options));
+router.options('*', cors(options));
 app.listen(PORT, async () => {
   console.log("Server listening on port " + PORT);
 });
