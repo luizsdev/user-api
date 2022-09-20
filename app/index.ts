2a0f3d84838c51d78ecc366fb59c2743b0bcfac5
luizsdev <luizsdev@gmail.com>
2022-09-20 03:06:52 -0300

    update

--- app/index.ts
+++ app/index.ts
@@ -15,11 +15,11 @@ const options: cors.CorsOptions = {
   ],
   credentials: true,
   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin: "https://portfolio-luizsdev.vercel.app",
+  origin: "https://portfolio-luizsdev.vercel.app/playground",
   preflightContinue: false,
 };
-router.use(cors(options));
-router.options('*', cors(options));
+app.options('*', cors(options));
+app.use(cors(options));
 app.listen(PORT, async () => {
   console.log("Server listening on port " + PORT);
 });
