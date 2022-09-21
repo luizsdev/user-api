4c8f3d6774e9b21bd84ecf8c4b1ec411abd54850
luizsdev <luizsdev@gmail.com>
2022-09-21 17:51:27 -0300

    test fixing

--- test/user.test.ts
+++ test/user.test.ts
@@ -1,2 +1,25 @@
-import express from "express";
-const app = express();
+import app from "../app/index"
+import supertest from "supertest"
+
+describe("GET ROUTES",()=>{
+    it('USERS REQUEST',async ()=>{
+        const result = await supertest(app).get("/users");
+        expect(result.statusCode).toBe(200);
+    })
+    it('USER BY ID',async ()=>{
+        const result = await supertest(app).get("/users/1")
+        expect(result.statusCode).toBe(200);
+    })
+})
+describe("POST ROUTES",()=>{
+    it("CREATE USER REQUEST",async ()=>{
+        const testData = {
+            "name":"John",
+            "user":"johnxd12",
+            "email":"johnxd12@gmail.com"
+        }
+        const result = await supertest(app).post("/createuser").send(testData)
+        expect(result.statusCode).toBe(200)
+
+    })
+})
