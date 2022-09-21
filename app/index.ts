f91074eed06ccf9a1cac124d060eab4686752db0
luizsdev <luizsdev@gmail.com>
2022-09-21 19:26:02 -0300

    fixing cors

--- app/index.ts
+++ app/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import router from "./Routes/userRoutes"
+import cors from "cors"
 const PORT = process.env.PORT || 5000
 const app = express();
-
+router.use(cors())
+app.use(cors())
 app.use(express.json());
 app.use("/", router);
 app.listen(PORT, async () => {
