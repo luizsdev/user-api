baa1e06d881bf7f3d51651f971a14641e1947ccb
luizsdev <luizsdev@gmail.com>
2022-09-21 17:55:36 -0300

    update port

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import router from "./Routes/userRoutes"
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(express.json());
